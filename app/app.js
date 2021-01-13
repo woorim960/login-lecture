892613c1ff5fd5fb7782f3ce66c9bcc61c27a18e
박우림 <woorimprog@gmail.com>
2021-01-13 22:02:02 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// 모듈
+const express = require("express");
+const app = express();
+
+// 라우팅
+const home = require("./src/routes/home");
+
+// 앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
+
+module.exports = app;
