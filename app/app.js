d0f03d8b83b8bb57784434c28cd8f15593e83d3a
박우림 <woorimprog@gmail.com>
2021-01-13 22:09:45 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
 
