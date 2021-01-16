2c782ef4e611d2847effaf2c5eae4acb38bbe707
박우림 <woorimprog@gmail.com>
2021-01-16 22:28:03 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -20,5 +20,14 @@ function login() {
     body: JSON.stringify(req),
   })
     .then((res) => res.json())
-    .then((res) => {});
+    .then((res) => {
+      if (res.success) {
+        location.href = "/";
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error("로그인 중 에러 발생");
+    });
 }
