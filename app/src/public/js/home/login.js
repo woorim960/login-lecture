b8470405b758b4a8681749057377cf096b642510
박우림 <woorimprog@gmail.com>
2021-01-16 21:39:47 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,4 +11,12 @@ function login() {
     id: id.value,
     psword: psword.value,
   };
+
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
 }
